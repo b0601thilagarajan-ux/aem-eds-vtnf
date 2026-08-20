f94735ffd95bdab6491b749ba739fc4d4bfda9fe
b0601thilagarajan-ux <b0601thilagarajan@gmail.com>
2026-08-20 23:04:55 +0530

    second-commit

--- blocks/card/card.js
+++ blocks/card/card.js
@@ -1,14 +1,14 @@
 export default function decorate(block) {
-    const cards = [...block.children];
-    cards.forEach((card) => {
-    card?.classList.add('section-card');
-    const cardimg = card.querySelector('p:has(img)');
-    cardimg?.classList.add('section-image');
-    const cardtitle = card.querySelector('h2');
-    cardtitle?.classList.add('section-title');
-    const cardtext = card.querySelector('p:not(:has(img))');
-    cardtext?.classList.add('section-text');
-    });
+  const cards = [...block.children];
+  cards.forEach((card) => {
+  card?.classList.add('section-card');
+  const cardimg = card.querySelector('p:has(img)');
+  cardimg?.classList.add('section-image');
+  const cardtitle = card.querySelector('h2');
+  cardtitle?.classList.add('section-title');
+  const cardtext = card.querySelector('p:not(:has(img))');
+  cardtext?.classList.add('section-text');
+  });
 }
  
 
